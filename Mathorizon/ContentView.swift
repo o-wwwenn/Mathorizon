23b9d3029839b09f559c535f9865dd74893faad8
Owen Yang <owwwenn829@gmail.com>
2026-03-13 01:19:23 +0800

    Initial Commit

---
+++ Mathorizon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mathorizon
+//
+//  Created by 楊哲鈞Owen on 2026/3/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mathorizon/MathorizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathorizonApp.swift
+//  Mathorizon
+//
+//  Created by 楊哲鈞Owen on 2026/3/13.
+//
+
+import SwiftUI
+
+@main
+struct MathorizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
